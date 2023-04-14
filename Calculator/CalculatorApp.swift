6a8b204dbe038b9532779ed6a0e1e7813d9d3c77
Tolgahan Sönmez <tolgahansonmez@outlook.com.tr>
2023-04-14 21:51:59 +0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by tolgahan sonmez on 14.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
